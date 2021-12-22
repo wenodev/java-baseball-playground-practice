760cdef6190d221b55fef7f2cda89c38a6e56ec9
wenodev <logicalstark1105@gmail.com>
2021-12-22 23:42:44 +0900

    test: String 클래스 요구사항1

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,17 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void test_1(){
+        // 준비
+        String actual = "1,2";
+
+        // 실행
+        String[] arr = actual.split(",");
+
+        // 검증
+        assertThat(arr).contains("1"); // 순서를 보장한다.
+        assertThat(arr).containsExactly("1"); // 순서를 보장하지 않는다.
+    }
 }
