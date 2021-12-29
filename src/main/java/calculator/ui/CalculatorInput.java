9450a4cdee0b04b30203764041dfb4fab3de689a
wenodev <logicalstark1105@gmail.com>
2021-12-30 08:26:17 +0900

    feat: CalculatorExecution를 생성하여 계산기를 실행할 수 있도록 합니다.

--- src/main/java/calculator/ui/CalculatorInput.java
+++ src/main/java/calculator/ui/CalculatorInput.java
@@ -6,7 +6,7 @@ public class CalculatorInput {
 
     public String generateStr() {
         Scanner scanner = new Scanner(System.in);
-        return scanner.next();
+        return scanner.nextLine();
     }
 
 }
