bc9a81bd48238bb6b465472c69dafcc5a7811e95
wenodev <logicalstark1105@gmail.com>
2021-12-31 23:51:00 +0900

    style: 첫 javadoc을 작성했습니다.

--- src/main/java/calculator/ui/CalculatorInput.java
+++ src/main/java/calculator/ui/CalculatorInput.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class CalculatorInput {
 
+    /**
+     * Scanner로 입력받은 문자를 리턴합니다
+     * @return 입력받은 문자
+     */
     public String generateStr() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
