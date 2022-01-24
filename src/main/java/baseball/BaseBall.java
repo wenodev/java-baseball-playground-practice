19f3661d25471cac16736a1cbbf77a49d3788a7a
wenodev <logicalstark1105@gmail.com>
2022-01-24 09:20:56 +0900

    test(BaseBall, BaseBallTest): generateRandomNumber 메소드를 테스트합니다.

--- src/main/java/baseball/BaseBall.java
+++ src/main/java/baseball/BaseBall.java
@@ -1,4 +1,11 @@
 package baseball;
 
+import java.util.Random;
+
 public class BaseBall {
+    public int generateRandomNumber() {
+        Random random = new Random();
+        random.setSeed(System.currentTimeMillis());
+        return (int) ((Math.random() * (1000 - 100)) + 100);
+    }
 }
--- src/test/java/baseball/BaseBallTest.java
+++ src/test/java/baseball/BaseBallTest.java
@@ -1,14 +1,31 @@
 package baseball;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BaseBallTest {
 
     private BaseBall baseBall;
 
-    @Test
+    @BeforeEach
     void setUp(){
         baseBall = new BaseBall();
+    }
+
+    @Test
+    void test_generate_random_number(){
+        // 준비
+
+        // 실행
+        int number = baseBall.generateRandomNumber();
+
+        // 검증
+        assertThat(number).isGreaterThan(100);
+        assertThat(number).isLessThan(1000);
     }
 
 }
