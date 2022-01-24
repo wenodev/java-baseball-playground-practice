760eebe41d3ef3a48d6e7018ae510f84f94e37a0
wenodev <logicalstark1105@gmail.com>
2022-01-24 09:07:09 +0900

    test(BaseBall, BaseBallTest): BaseBall 클래스를 생성을 테스트 합니다.

---
+++ src/test/java/baseball/BaseBallTest.java
@@ -0,0 +1,14 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+public class BaseBallTest {
+
+    private BaseBall baseBall;
+
+    @Test
+    void setUp(){
+        baseBall = new BaseBall();
+    }
+
+}
