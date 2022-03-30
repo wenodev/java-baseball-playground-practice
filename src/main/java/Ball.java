eae75d84aed1f2fee032c0c3f3864d22f4b8b731
wenodev <logicalstark1105@gmail.com>
2022-03-31 01:00:54 +0900

    feat(Ball): Ball 생성합니다

---
+++ src/main/java/Ball.java
@@ -0,0 +1,18 @@
+public class Ball {
+
+    private int position;
+    private int number;
+
+    public Ball(int position, int number) {
+        this.position = position;
+        this.number = number;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
